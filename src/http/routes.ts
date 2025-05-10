fc2053fad1ecffa347b4c90626d1a27a057ddeda
Guilherme Bezerra <gbezerradev@gmail.com>
2025-05-10 01:57:27 -0300

    feat: implement user registration functionality with Prisma and bcrypt

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,7 +1,10 @@
 import { FastifyInstance } from 'fastify'
+import { register } from './controllers/register'
 
 export async function appRoutes(app: FastifyInstance) {
   app.get('/health', async () => {
     return { status: 'ok' }
   })
+
+  app.post('/register', register)
 }
