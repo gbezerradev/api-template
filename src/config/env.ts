abdc7209fc8b70450d9ac212588222b4760281f1
Guilherme Bezerra <guilherme10alves.ga@gmail.com>
2025-05-12 15:41:49 -0300

    feat: update environment configuration and add @fastify/jwt dependency

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,6 +6,7 @@ const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   DATABASE_URL: z.string(),
   HASH_SALT_ROUNDS: z.coerce.number().default(12),
+  JWT_SECRET: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
